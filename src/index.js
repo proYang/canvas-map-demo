6eafc15f872e9740bf1801e6c133264c4cc73d19
SlaneYang <904509221@qq.com>
2017-12-18 11:58:25 +0800

    改回添加机器人功能

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import {
 import {
     randomNum,
     randomColor,
+    addClass,
+    removeClass,
     deepClone
 } from 'outils';
 const $app = document.querySelector('#app')
@@ -17,48 +19,65 @@ const options = {
 }
 let app = new App(options)
 
-function mockPeopleServer() {
-    let people = []
+// function mockPeopleServer() {
+//     let people = []
+//     let {
+//         width,
+//         height
+//     } = $app.getBoundingClientRect();
+//     let peopleNum = randomNum(5, 15)
+//     // 添加人
+//     let id = 1000
+//     for (let i = 0; i < peopleNum; i++) {
+//         let x = randomNum(0, width)
+//         let y = randomNum(0, height)
+//         people.push({
+//             id: id++,
+//             imgIndex: randomNum(0, 2),
+//             name: Random.cname(),
+//             color: randomColor(),
+//             move: [{
+//                 x,
+//                 y
+//             }]
+//         })
+//     }
+//     // 模拟运动轨迹
+//     people.forEach((person, index) => {
+//         let moveNum = randomNum(50, 100)
+//         for (let i = 0; i < moveNum; i++) {
+//             let last = person.move[person.move.length - 1]
+//             let next = {
+//                 x: last.x + randomNum(-12, 12),
+//                 y: last.y + randomNum(-12, 12)
+//             }
+//             people[index].move.push(next)
+//         }
+//     });
+//     return people
+// }
+// let mockPeople = mockPeopleServer()
+// 添加机器人
+document.querySelector('.J_add').addEventListener('click', () => {
+
+    // app.options.people = mockPeople
     let {
         width,
         height
     } = $app.getBoundingClientRect();
-    let peopleNum = randomNum(5, 15)
-    // 添加人
     let id = 1000
-    for (let i = 0; i < peopleNum; i++) {
-        let x = randomNum(0, width)
-        let y = randomNum(0, height)
-        people.push({
-            id: id++,
-            imgIndex: randomNum(0, 2),
-            name: Random.cname(),
-            color: randomColor(),
-            move: [{
-                x,
-                y
-            }]
-        })
-    }
-    // 模拟运动轨迹
-    people.forEach((person, index) => {
-        let moveNum = randomNum(50, 100)
-        for (let i = 0; i < moveNum; i++) {
-            let last = person.move[person.move.length - 1]
-            let next = {
-                x: last.x + randomNum(-12, 12),
-                y: last.y + randomNum(-12, 12)
-            }
-            people[index].move.push(next)
-        }
-    });
-    return people
-}
-let mockPeople = mockPeopleServer()
-// 实时位置
-document.querySelector('.J_add').addEventListener('click', () => {
-
-    app.options.people = mockPeople
+    let x = randomNum(0, width)
+    let y = randomNum(0, height)
+    app.options.people.push({
+        id: id++,
+        imgIndex: randomNum(0, 2),
+        name: Random.cname(),
+        color: randomColor(),
+        move: [{
+            x,
+            y
+        }]
+    })
     // 重绘轨迹，人
     // app.updateCanvas('people')
     // app.updateCanvas('move')
@@ -66,10 +85,20 @@ document.querySelector('.J_add').addEventListener('click', () => {
 
 // 是否展示运动轨迹
 // let mockServer4
-document.querySelector('.J_show').addEventListener('click', () => {
-    document.querySelector('.J_add').click()
+let $show = document.querySelector('.J_show');
+$show.addEventListener('click', () => {
+    // document.querySelector('.J_add').click()
     // clearInterval(mockServer4)
     app.showPath = !app.showPath
+    if (app.showPath === false) {
+        $show.innerHTML = '运动轨迹'
+        removeClass($show, 'btn-warning')
+        addClass($show, 'btn-primary')
+    } else {
+        $show.innerHTML = '关闭轨迹'
+        removeClass($show, 'btn-primary')
+        addClass($show, 'btn-warning')
+    }
 
     // let people = deepClone(app.options.people);
     // app.options.people.forEach((person, index, array) => {
@@ -112,39 +141,39 @@ $measureCancel.addEventListener('click', () => {
 })
 
 // 数据模拟器
-// let mockServer1 = setInterval(() => {
-//     app.options.people.forEach((person, index) => {
-//         let length = person.move.length
-//         let last = person.move[--length]
-//         let next = {
-//             x: last.x + randomNum(-10, 10),
-//             y: last.y + randomNum(-10, 10)
-//         }
-//         app.options.people[index].move.push(next)
-//         if (length > 70) app.options.people[index].move.shift()
-//     });
-// }, 500)
-// let mockServer2 = setInterval(() => {
-//     app.options.people.forEach((person, index) => {
-//         let length = person.move.length
-//         let last = person.move[--length]
-//         let next = {
-//             x: last.x + randomNum(-10, 10),
-//             y: last.y + randomNum(-10, 10)
-//         }
-//         app.options.people[index].move.push(next)
-//         if (length > 70) app.options.people[index].move.shift()
-//     });
-// }, 300)
-// let mockServer3 = setInterval(() => {
-//     app.options.people.forEach((person, index) => {
-//         let length = person.move.length
-//         let last = person.move[--length]
-//         let next = {
-//             x: last.x + randomNum(-10, 10),
-//             y: last.y + randomNum(-10, 10)
-//         }
-//         app.options.people[index].move.push(next)
-//         if (length > 70) app.options.people[index].move.shift()
-//     });
-// }, 1000)
+let mockServer1 = setInterval(() => {
+    app.options.people.forEach((person, index) => {
+        let length = person.move.length
+        let last = person.move[--length]
+        let next = {
+            x: last.x + randomNum(-5, 5),
+            y: last.y + randomNum(-5, 5)
+        }
+        app.options.people[index].move.push(next)
+        if (length > 100) app.options.people[index].move.shift()
+    });
+}, 500)
+let mockServer2 = setInterval(() => {
+    app.options.people.forEach((person, index) => {
+        let length = person.move.length
+        let last = person.move[--length]
+        let next = {
+            x: last.x + randomNum(-5, 5),
+            y: last.y + randomNum(-5, 5)
+        }
+        app.options.people[index].move.push(next)
+        if (length > 100) app.options.people[index].move.shift()
+    });
+}, 300)
+let mockServer3 = setInterval(() => {
+    app.options.people.forEach((person, index) => {
+        let length = person.move.length
+        let last = person.move[--length]
+        let next = {
+            x: last.x + randomNum(-5, 5),
+            y: last.y + randomNum(-5, 5)
+        }
+        app.options.people[index].move.push(next)
+        if (length > 100) app.options.people[index].move.shift()
+    });
+}, 1000)
